cef20921ceb7bd1a9081e5bbd02e1dc3d87ff7d6
Kate <katemhagen@gmail.com>
2024-06-15 20:26:33 -0700

    test stage 2

--- app/main.py
+++ app/main.py
@@ -5,10 +5,14 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    pong = "+PONG\r\n"
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
     client, addr = server_socket.accept()
-    client.send(b"+PONG\r\n")
+
+    with client:
+        client.recv(1024)
+        client.send(pong.encode())
 
 if __name__ == "__main__":
     main()
