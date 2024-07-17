25a3fe0c2a8d2fdc4efe8c41943b2b55705d0a7b
Kate <katemhagen@gmail.com>
2024-07-16 18:32:43 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,148 +1,208 @@
+from collections import defaultdict
+from dataclasses import dataclass
+import time
+from typing import List, Optional
 import asyncio
 import argparse
 
-dict = {}
-expiry_of_tasks = {}
-is_slave = False
+# Using data class because I want to store data, not use logic
+@dataclass
+class Item:
+    value: str
+    expiry: Optional[int] = None
+
+store = defaultdict(Item)
 replicas = []
+replica_writers = []
 
-def parse_resp(data):
-    if data.startswith(b'*'):
-        # Split data in parts based on \r\n
-        # Extract num of items
-        parts = data.split(b'\r\n')
-        arr = int(parts[0][1:])
-        new_arr = [] 
-        i = 1
-        # Loop through each part to parse RESP protocol
-        while arr > 0:
-            if parts[i].startswith(b'$'):
-                # Extract length of following item
-                length = int(parts[i][1:])
-                new_arr.append(parts[i+1][:length])
-                # Move to new item skipping length and data
-                i += 2
-                # Decrease remaining item count
-                arr -= 1
-        return new_arr 
-    return []
-   
+WRITE_COMMANDS = ["SET"]
 
+replica_port = None
+replica_reader = None
+master_port = None
+
+
+async def handle_message(data, writer):
+    global master_port, port, replica_port
+    print("from handle client")
+    print("data: " + data)
+  
+    if data:
+        data_split = data.split("\r\n")
+        print(data_split)
+        if len(data_split) > 2:
+            cmd = data_split[2].upper()
+
+            if "PING" in cmd:
+                writer.write('+PONG\r\n'.encode())
+                await writer.drain()
+            elif "ECHO" in cmd:
+                resp = data_split[-2]
+                writer.write(f"${len(resp)}\r\n{resp}\r\n".encode())
+                await writer.drain()
+            elif "SET" in cmd:
+                key = data_split[4]
+                value = data_split[6]
+              
+                if len(data_split) == 12:
+                    # Expire = current time in ms + additional time
+                    expire = int(time.time_ns() // 10**6) + int(data_split[10])
+                else:
+                    expire = None
+                
+                store[key] = Item(value, expire) 
+                writer.write("+OK\r\n".encode())
+                await writer.drain()
+            elif "GET" in cmd:
+                key = data_split[4]
+                store_item: Optional[Item] = store.get(key)
+                print(f"store {store}")
+
+                if (
+                    # If store and value exists
+                    store
+                    and store_item.value
+                    and (
+                        store_item.expiry is None # No expiry
+                        or (
+                            # Expiry is in the future
+                            store_item.expiry is not None
+                            and store_item.expiry > (time.time_ns() // 10**6)
+                        )
+                    )
+                ):
+                    writer.write(f"${len(store_item.value)}\r\n{store_item.value}\r\n".encode())
+                else:
+                    writer.write("$-1\r\n".encode())
+                await writer.drain()
+            elif "INFO" in cmd: # Respond to INFO replication command
+                info_type = data_split[4] if len(data_split) == 6 else None
+                if info_type and info_type == "replication":
+                    role = "slave" if args.replicaof else "master"
+                    role_str = f"role:{role}"
+                    if role == "master":
+                        master_replid = "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+                        master_repl_offset = "master_repl_offset:0"
+                        master_info_str = f"{role_str}\n{master_replid}\n{master_repl_offset}"
+                        writer.write(f"${len(master_info_str)}\r\n{master_info_str}\r\n".encode())
+                    else:
+                        writer.write(f"${len(role_str)}\r\n{role_str}\r\n".encode())
+                    await  writer.drain()
+                else:
+                    writer.write("$-1\r\n".encode())
+                    await writer.drain()
+            elif "REPLCONF" in cmd: # Respond to REPLCONF command
+                if "listening-port" in data_split:
+                    replica_port = data_split[6]
+                writer.write('+OK\r\n'.encode())
+                await writer.drain()
+            elif "PSYNC" in cmd:
+                global replica_writer
+                fullresync = '+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n'
+                # Send empty RDB file
+                rdb_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
+                binary_data = bytes.fromhex(rdb_hex)
+                header = f"${len(binary_data)}\r\n"
+
+                writer.write(fullresync.encode())
+                await writer.drain()
+                writer.write(header.encode() + binary_data)
+                await writer.drain()
+                replica_writers.append(writer)
+                await writer.drain()
+            
+            if master_port == port and replica_port and cmd in WRITE_COMMANDS:
+                await propagate_commands(data)
+
+async def propagate_commands(data):
+        global replica_writers
+        if data:
+            for writer in replica_writers:
+                if writer.is_closing():
+                    break
+                writer.write(data.encode())
+                await writer.drain()
+  
 async def handle_client(reader, writer):
-    global replicas
-    
-    
-
     while True:
-        # Read client data
         data = await reader.read(1024)
-        # Parse received data
-        parse = parse_resp(data)
-        # Decode first command from parsed RESP data
-        command = parse[0].decode()
-      
-        if command == 'PING': # Respond to PING command
-            writer.write(b'+PONG\r\n')
-        elif command == 'ECHO': # Respond to ECHO command
-            # The tester will expect to receive $3\r\nhey\r\n as a response (that's the string hey encoded as a RESP bulk string.
-            if len(parse) > 1:
-                response = f"${len(parse[1])}\r\n{parse[1].decode()}\r\n".encode()
-                writer.write(response)
-        elif command == 'SET': # Respond to SET command
-            expiry = None
-            # Handle if key has an expiry
-            if len(parse) > 3 and parse[3] == b'px':
-                expiry = int(parse[4])
-        
-            dict[parse[1]] = parse[2]   
+        decoded_msg = data.decode()
+        if not decoded_msg:
+            break
+        await handle_message(decoded_msg, writer)
+    writer.close()
 
-            if expiry:
-                if parse[1] in expiry_of_tasks:
-                    expiry_of_tasks[parse[1]].cancel()
-                expiry_of_tasks[parse[1]] = asyncio.create_task(expire_key(parse[1], expiry))
-            
-            # Propagating write command from master to replica
-            for replica in replicas:
-                replica.write(data)
-                await replica.drain()
-            
-            response = "+OK\r\n".encode()
-            writer.write(response)
-          
-        elif command == 'GET': # Respond to GET command
-            if parse[1] in dict:
-                response = f"${len(dict[parse[1]])}\r\n{dict[parse[1]].decode()}\r\n".encode()
-            else:
-                response = b"$-1\r\n"
-            writer.write(response)
-        elif command == 'INFO': # Respond to INFO replication command
-            if is_slave:
-                response = b'$10\r\nrole:slave\r\n'
-            else:
-                response = b'$89\r\nrole:master\r\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\r\nmaster_repl_offset:0\r\n'
-            writer.write(response)
-        elif command == 'REPLCONF': # Respond to REPLCONF command
-            writer.write(b'+OK\r\n')
-        elif command == 'PSYNC': # Respond to PSYNC command
-            replicas.append(writer)
-            writer.write(b'+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n')
-            # Send empty RDB file
-            rdb_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
-            rdb_content = bytes.fromhex(rdb_hex)
-            rdb_data = f'${len(rdb_content)}\r\n'.encode()
-            writer.write(rdb_data + rdb_content)
-        await writer.drain() # Ensure data is written to client
-
-async def expire_key(key, expiry):
-    # Convert milliseconds to seconds
-    await asyncio.sleep(expiry / 1000)
-    # Removes key if it exists, returns None if it doesn.t
-    dict.pop(key, None)
-    expiry_of_tasks.pop(key, None)
-    
-async def connect_to_master(master_host, master_port):
+async def handle_handshake(reader, writer):
     """
     Send handshake.
     Connects to the master server if the current server is a replica.
     The replica sends a PING command to the master server, then sends REPLCONF twice to the master,
     and lastly a PSYNC to the master.
     """
-    reader, writer = await asyncio.open_connection(master_host, master_port)
+    print('from run handshake')
+    replicas.append(writer)
+    # reader, writer = await asyncio.open_connection(master_host, master_port)
     writer.write(b'*1\r\n$4\r\nPING\r\n')
     await reader.read(1024)
+
     writer.write(b'*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n'
     )
     await reader.read(1024)
+
     writer.write(b'*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n')
     await reader.read(1024)
     # PSYNC ? -1 is the replicas way of telling the master that it doesn't have any data yet, and
     # needs to be fully resynchronized
     writer.write(b'*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n')
-    
+    replicas.append(writer)
+    # For RDB file
+    await reader.read(1024)
+    await reader.read(1024)
+    print("Recieved RDB file. Handshake complete")
     await writer.drain()
-    
+
+async def connect_master(replica):
+    host, port = replica.split(" ")
+    reader_writer = await asyncio.open_connection(host, port)
+    reader = reader_writer[0]
+    writer = reader_writer[1]
+    await handle_handshake(reader, writer)
+
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            break
+        msg1 = data.decode()
+        raw_reqs = list(msg1.split("*"))[1:]
+        for message in raw_reqs:
+            message = "*" + message
+            await handle_message(message, writer)
+    writer.close()
+    await writer.wait_closed()
 
 async def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    global is_slave
+    global port, master_host, master_port, args
     # Start TCP server listening on localhost and port specified or default to 6379 if port is not specified
     parser = argparse.ArgumentParser("A Redis server written in Python")
-    parser.add_argument('--port', type=int, default=6379)
-    parser.add_argument('--replicaof', type=ascii)
+    parser.add_argument('--port', type=int, default=6379, help="Port to run the server on")
+    parser.add_argument('--replicaof', type=str, default=None, help="Replica server and port")
     
     args = parser.parse_args()
-   
-    if args.replicaof:
-        is_slave = True
-        master_host = parser.parse_args().replicaof[1:10]
-        master_port = parser.parse_args().replicaof[11:15]
-        await connect_to_master(master_host, master_port)
-       
-    server_socket = await asyncio.start_server(handle_client, "localhost", args.port)
-
-    async with server_socket:
-        await server_socket.serve_forever() # Serve clients forever
+    port = args.port
+    master_port = port
+    server_socket = await asyncio.start_server(handle_client, "localhost", port)
     
+    try:
+        if args.replicaof:
+            await connect_master(args.replicaof)
+        async with server_socket:
+            await server_socket.serve_forever()
+    finally:
+        server_socket.close()
+
+
+        
 if __name__ == "__main__":
     asyncio.run(main())
